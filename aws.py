088ddc2d8465c237bc6e06a0cd7e1c2db1baefa6
sauravrana646 <42442467+sauravrana646@users.noreply.github.com>
2020-11-05 02:09:46 +0530

    test

--- aws.py
+++ aws.py
@@ -1,13 +1,34 @@
 import os
 from welcome import welcome
+from colorama import Fore, Back, Style
+from subprocess import run, PIPE
 
 
 def aws():
+    os.system("clear")
+    print(welcome("AWS"))
+    print("First you need to login to aws\n")
+    auth = run("aws configure", stderr=PIPE)
+    authout = run("aws sts get-caller-identity", capture_output=True)
+    if auth.returncode == 0:
+        print(Fore.GREEN +
+              f"\nAuthentication succcess\n\n{authout.stdout.decode()}")
+        print(Style.RESET_ALL)
+        input("Press ENTER to continue....")
+    else:
+        print(
+            Fore.RED + f"\nCouldn't authenticate\n\nError : \n{authout.stderr.decode()}")
+        print(Style.RESET_ALL)
+        input("Press ENTER to continue....")
+
     while True:
         os.system("clear")
         print(welcome("AWS"))
         print("""Select from below : 
-        1. 
+        1. EC2
+        2. S3
+        3. Cloundfront
+        4. 
         9. Go back""")
         choice = int(input("\nEnter your choice : "))
         if choice == 9:
