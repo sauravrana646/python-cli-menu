7a15a35daf5a6df3e67d1c52c166013f4bda8369
Prakhar Khandelwal <56904319+prakhar101@users.noreply.github.com>
2020-11-08 20:40:38 +0530

    Add function docker having for menu driven
    Added a function docker for having menu driven functionality...

--- docker.py
+++ docker.py
@@ -1,6 +1,8 @@
-import subprocess
+import subprocess # for  running the os level commands
+import platform #for finding the name of the os
+from welcome import welcome
 def pull_image(): #Method for pulling/downloading the docker image form the docker hub
-   
+
     image=input("Enter the image name[also specify the version if you want]-:")
     o=subprocess.run("dokcer pull {}".image,shell=True)
     if o.returncode ==0:
@@ -9,7 +11,7 @@ def pull_image(): #Method for pulling/downloading the docker image form the dock
         print("Some error happened..:(")
 
 def launch_container(): #Method for launching the docker container 
-   
+
     image_name=input("Enter the image name-:")
     os_name=input("Enter the name you want to give[else just press enter]-:")
     if len(os_name) !=0:
@@ -18,7 +20,7 @@ def launch_container(): #Method for launching the docker container
         subprocess.run("docker run -it {}".format(image_name),shell=True)
 
 def remove_image(): #Method for removing the image
-   
+
     image_name=input("Enter the image name you want to delete-:")
     o=subprocess.run("doker rmi -f {}".format(image_name),shell=True)
     if o.returncode ==0:
@@ -28,7 +30,7 @@ def remove_image(): #Method for removing the image
 
 
 def remove_container(): #method for removing the container
-  
+
     image_or_id_of_os=input("Enter the image or id of the container/os you want to delete-:")
     o=subprocess.run("docker rm {}".format(image_or_id_of_os),shell=True)
     if o.returncode ==0:
@@ -37,13 +39,13 @@ def remove_container(): #method for removing the container
         print("Some error happened..:(")
 
 def see_logs(): # Method for seeing the logs of a container
-  
+
     image_or_id_of_os=input("Enter the name or id of the conatiner/os whose logs you want to see-:")
     subprocess.run("docker logs {}".format(image_or_id_of_os),shell=True)
     
  
 def cp_base_to_cont(): #Method for copying the content from the base os to the conatiner
-   
+
     content=input("Enter the path of the content you want to copy from os to container-:")
     os=input("Enter the os name or id-:")
     path_in_os=input("Enter the location in the conatiner where you want to store the content-:")
@@ -54,7 +56,7 @@ def cp_base_to_cont(): #Method for copying the content from the base os to the c
         print("Some error happened..:(")
 
 def cp_cont_to_base(): #Method for copying the content  from container to the base os
-    
+
     content=input("Enter the path of the content you want to copy from container  to base os-:")
     os=input("Enter the os name or id-:")
     path_in_base=input("Enter the loaction where you want to copy the content in the base os-;")
@@ -66,12 +68,12 @@ def cp_cont_to_base(): #Method for copying the content  from container to the ba
 
 
 def see_the_running_cont(): #Method for seeing the currently running container
- 
+
     subprocess.run("docker ps ",shell=True)
 
 
 def see_all_cont(): #Method for seeing the all [active+inactive] containers
-   
+
     subprocess.run("docker ps -a",shell=True)
 
 
@@ -79,25 +81,107 @@ def see_all_cont(): #Method for seeing the all [active+inactive] containers
 
 
 def start_exist_cont(): #Method for starting the existing container..
-   
+
     name_id_os=input("Enter the name/id of the conatiner/os-:")
-    subprocess.run("docker start {}".format(name_id_os),shell=True)
-    choice=input("Do you want to to get the terminal of this conatiner [y/n]? ")
-    if choice=="y":
+    code=subprocess.run("docker start {}".format(name_id_os),shell=True)
+    if code.returncode==0:
+     print("Conatiner start Successfully..:)")
+     choice=input("Do you want to to get the terminal of this conatiner [y/n]? ")
+     if choice=="y":
         get_terminal(name_id_os)
+    else:
+        print("Something wrong happenmed....:(")
 
 def get_terminal(os="none"): #method for getting the terminal of the launched container
-    
+
     if os=="none":
         os=input("Enter the name/id of the conatiner/os -:")
     subprocess.run("docker attach {}".format(os),shell=True)
 
 
 def start_docker_service(): # Method for starting the docker service
-  
-    subprocess.run("systemctl start docker",shell=True)
+
+    code=subprocess.run("systemctl start docker",shell=True)
+    if code.returncode==0:
+        print("Service start Successfully...:)")
+    else:
+        print("Something wrong happened..:(")
+
+def stop_docker_services(): #method for stopping the docker service
+    code=subprocess.run("systemctl stop docker",shell=True)
+    if code.returncode==0:
+        print("Service stop Successfully...:)")
+    else:
+        print("Something wrong happened..:(")
 
 def stop_running_docker():  #method for stopping the running container
-    
+
     os=input("Enter the os/container name/id -:")
-    subprocess.run("docker stop {}".format(os),shell=True)
+    code=subprocess.run("docker stop {}".format(os),shell=True)
+    if code.returncode==0:
+        print("Conatiner stopped Successfully..:)")
+    else:
+        print("Something wrong happpened..:(")
+
+
+
+def docker():
+  while True:
+    print(welcome("Docker"))
+    print("""    1. Launch a new Conatiner
+    2. Pull/Download image
+    3. Remove image
+    4. Remove Container
+    5. See Logs of a Container
+    6. Copy Content   from Base OS to Container
+    7. Copy Content from Conatiner to Base OS
+    8. See the list of all running Containers
+    9. See list of all Containers[active and inactive]
+   10. Start existing Container
+   11. Get terminal of Existing Container
+   12. Start Docker Services
+   13. Stop running  Container
+   14. Stop Docker Services.
+   15. Go Back
+
+    
+""")
+    choice=int(input("Enter your choice-:"))
+    if choice ==1:
+        launch_container()
+    elif choice==2:
+        pull_image()
+    elif choice ==3:
+        remove_image()
+    elif choice==4:
+        remove_container()
+    elif choice==5:
+        see_logs()
+    elif choice==6:
+        cp_base_to_cont()
+    elif choice==7:
+        cp_cont_to_base()
+    elif choice ==8:
+        see_the_running_cont()
+    elif choice==9:
+        see_all_cont()
+    elif choice ==10:
+        start_exist_cont()
+    elif choice==11:
+        get_terminal()
+    elif choice==12:
+        start_docker_service()
+    elif choice ==13:
+        stop_running_docker()
+    elif choice==14:
+        stop_docker_services()
+    elif choice==15:
+        return
+
+    input("\n\nPress any key to continue...")
+    os_name=platform.system().lower()
+    if os_name=="linux":
+        subprocess.run("clear",shell=True)
+    elif os_name=="windows":
+        subprocess.run("cls",shell=True)
+
